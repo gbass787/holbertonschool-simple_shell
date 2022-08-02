2327d4a8b82ce429abe7360d94f6aba39d4cf263
spcruz5 <spcruz5@gmail.com>
2022-08-02 21:50:07 +0000

    header file

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+char *_strcat(char *dest, char *src);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+int _strncmp(char *s1, char *s2, int n);
+int _strcmp(char *s1, char *s2);
+void _print_int(int number);
+int _putchar(char c);
+
+#endif
