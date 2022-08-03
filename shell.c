e056b661db0fd6ff309fd73e0452ef40a245be24
spcruz5 <spcruz5@gmail.com>
2022-08-03 21:11:22 +0000

    Main function updated

--- shell.c
+++ shell.c
@@ -1,25 +1,52 @@
 #include "main.h"
+/**
+ * main - simple shell
+ * @ac: unused
+ * @av: unused
+ * @env: unused
+ * Return: 1
+ */
 
 int main(int ac, char **av, char **env)
 {
 	char *lineptr = NULL;
 	size_t n = 0;
 	char **str;
-	
+	pid_t child_pid;
+	int status;
+
 	(void)env;
 	(void)ac;
 	(void)av;
 	(void)str;
 
-	do{
+	while (1){
+		printf("$");
 
-	printf("$");
+		getline(&lineptr, &n, stdin);
 
-	getline(&lineptr, &n, stdin);
+		if (_strcmp(lineptr, "exit\n") == 0)
+		{
+			free(lineptr);
+			exit(0);
+		}
+		str = split_line(lineptr);
 
-	str = split_line(lineptr);
-
-	}while (1);
-
+		if( str[0] != NULL){
+			child_pid = fork();
+			if (child_pid != 0)
+			{
+				wait (&status);
+				free(str);
+			}
+			else
+			{
+				execve(str[0], str, env);
+				return(0);
+			}
+		}
+	}
+	free(str);
+	free(lineptr);
 	return (1);
 }
