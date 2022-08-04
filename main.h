fca107ebf9a41b83551100a1d5d8fec0ed0db4f2
gbass787 <4551@holbertonschool.com>
2022-08-04 11:54:30 +0000

    added additional libraries to main.h

--- main.h
+++ main.h
@@ -6,7 +6,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
 #include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
 
 char **split_line(char *lineptr);
 
